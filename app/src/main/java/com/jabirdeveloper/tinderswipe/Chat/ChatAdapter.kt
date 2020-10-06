68f8e3ce2db822d069691267b8b6ace795440e5a
katawong0@gmail.com <sotgame123>
2020-10-06 21:09:21 +0700

    update By Guy

--- app/src/main/java/com/jabirdeveloper/tinderswipe/Chat/ChatAdapter.kt
+++ app/src/main/java/com/jabirdeveloper/tinderswipe/Chat/ChatAdapter.kt
@@ -13,6 +13,7 @@ import com.bumptech.glide.Glide
 import com.jabirdeveloper.tinderswipe.R
 
 class ChatAdapter(private val chatList: MutableList<ChatObject?>?, private val context: Context?) : RecyclerView.Adapter<ChatViewHolders?>() {
+    @SuppressLint("InflateParams")
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatViewHolders {
         val layoutView = LayoutInflater.from(parent.context).inflate(R.layout.item_chat, null, false)
         val lp = RecyclerView.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
@@ -25,16 +26,16 @@ class ChatAdapter(private val chatList: MutableList<ChatObject?>?, private val c
         holder.mchk3.visibility = View.GONE
         holder.mchk2.visibility = View.GONE
         holder.mchk.visibility = View.GONE
-        holder.mImage_opposite.visibility = View.GONE
+        holder.imageOpposite.visibility = View.GONE
         holder.mMatchIdReal.text = chatList!![position]!!.createByUser
         holder.mMatchId.text = chatList[position]!!.Match_id
         holder.mChk_2.text = "" + chatList[position]!!.chk
         holder.mChk.text = "" + chatList[chatList.size - 1]!!.chk
-        holder.time_send.text = chatList[position]!!.time
+        holder.timeSend.text = chatList[position]!!.time
         if (chatList[position]!!.currentUser!!) {
             when {
                 chatList[position]!!.url !== "default" -> {
-                    holder.mImage_opposite.visibility = View.GONE
+                    holder.imageOpposite.visibility = View.GONE
                     holder.mchk3.visibility = View.VISIBLE
                     holder.mchk.visibility = View.VISIBLE
                     val params3 = holder.mchk.layoutParams as RelativeLayout.LayoutParams
@@ -48,11 +49,11 @@ class ChatAdapter(private val chatList: MutableList<ChatObject?>?, private val c
                     holder.mchk2.visibility = View.GONE
                 }
                 chatList[position]!!.audio_Url != "null" -> {
-                    holder.button_audio.visibility = View.VISIBLE
-                    holder.CD!!.visibility = View.VISIBLE
-                    holder.begin_audio.visibility = View.VISIBLE
+                    holder.buttonAudio.visibility = View.VISIBLE
+                    holder.cd.visibility = View.VISIBLE
+                    holder.beginAudio.visibility = View.VISIBLE
                     if (Integer.valueOf(chatList[position]!!.audio_length!!) < 60) {
-                        holder.audio_url.text = chatList.elementAt(position)!!.audio_Url
+                        holder.audioUrl.text = chatList.elementAt(position)!!.audio_Url
                         val second = String.format("%02d", Integer.valueOf(chatList[position]!!.audio_length!!))
                         holder.mMessage.text = "00:$second"
                     } else {
@@ -60,7 +61,7 @@ class ChatAdapter(private val chatList: MutableList<ChatObject?>?, private val c
                         val second = String.format("%02d", Integer.valueOf(chatList[position]!!.audio_length!!) % 60)
                         holder.mMessage.text = "$minute:$second"
                     }
-                    holder.begin_audio.setTextColor(Color.parseColor("#FFFFFF"))
+                    holder.beginAudio.setTextColor(Color.parseColor("#FFFFFF"))
                     holder.mMessage.setTextColor(Color.parseColor("#FFFFFF"))
                     holder.mchk3.visibility = View.GONE
                     holder.mchk.visibility = View.VISIBLE
@@ -71,15 +72,15 @@ class ChatAdapter(private val chatList: MutableList<ChatObject?>?, private val c
                     params3.addRule(RelativeLayout.END_OF, 0)
                     params3.addRule(RelativeLayout.START_OF, holder.mchk2.id)
                     params3.addRule(RelativeLayout.ALIGN_BOTTOM, holder.mchk2.id)
-                    holder.mImage_opposite.visibility = View.GONE
+                    holder.imageOpposite.visibility = View.GONE
                     holder.mchk2.background = ContextCompat.getDrawable(context!!, R.drawable.chat_1)
                 }
                 else -> {
                     holder.mMessage.text = chatList[position]!!.message
-                    holder.button_audio.visibility = View.GONE
-                    holder.CD!!.visibility = View.GONE
-                    holder.begin_audio.visibility = View.GONE
-                    holder.begin_audio.setTextColor(Color.parseColor("#FFFFFF"))
+                    holder.buttonAudio.visibility = View.GONE
+                    holder.cd.visibility = View.GONE
+                    holder.beginAudio.visibility = View.GONE
+                    holder.beginAudio.setTextColor(Color.parseColor("#FFFFFF"))
                     holder.mMessage.setTextColor(Color.parseColor("#FFFFFF"))
                     holder.mchk3.visibility = View.GONE
                     holder.mchk.visibility = View.VISIBLE
@@ -90,7 +91,7 @@ class ChatAdapter(private val chatList: MutableList<ChatObject?>?, private val c
                     params3.addRule(RelativeLayout.END_OF, 0)
                     params3.addRule(RelativeLayout.START_OF, holder.mchk2.id)
                     params3.addRule(RelativeLayout.ALIGN_BOTTOM, holder.mchk2.id)
-                    holder.mImage_opposite.visibility = View.GONE
+                    holder.imageOpposite.visibility = View.GONE
                     holder.mchk2.background = context?.let { ContextCompat.getDrawable(it, R.drawable.chat_1) }
                 }
             }
@@ -105,18 +106,18 @@ class ChatAdapter(private val chatList: MutableList<ChatObject?>?, private val c
                     params.addRule(RelativeLayout.ALIGN_BOTTOM, holder.mchk3.id)
                     val paramsimg = holder.mchk3.layoutParams as RelativeLayout.LayoutParams
                     paramsimg.addRule(RelativeLayout.ALIGN_PARENT_END, 0)
-                    holder.mImage_opposite.visibility = View.VISIBLE
-                    Glide.with(context!!).load(chatList[position]!!.profileImageUrl).into(holder.mImage_opposite)
+                    holder.imageOpposite.visibility = View.VISIBLE
+                    Glide.with(context!!).load(chatList[position]!!.profileImageUrl).into(holder.imageOpposite)
                     Glide.with(context).load(chatList[position]!!.url).into(holder.mImage_sent)
                     holder.mchk3.background = ContextCompat.getDrawable(context, R.drawable.chat_2_photo)
                     holder.mchk2.visibility = View.GONE
                 }
                 chatList[position]!!.audio_Url != "null" -> {
-                    holder.button_audio.visibility = View.VISIBLE
-                    holder.CD!!.visibility = View.VISIBLE
-                    holder.begin_audio.visibility = View.VISIBLE
+                    holder.buttonAudio.visibility = View.VISIBLE
+                    holder.cd.visibility = View.VISIBLE
+                    holder.beginAudio.visibility = View.VISIBLE
                     if (Integer.valueOf(chatList[position]!!.audio_length!!) < 60) {
-                        holder.audio_url.text = chatList[position]!!.audio_Url
+                        holder.audioUrl.text = chatList[position]!!.audio_Url
                         val second = String.format("%02d", Integer.valueOf(chatList[position]!!.audio_length!!))
                         holder.mMessage.text = "00:$second"
                     } else {
@@ -124,7 +125,7 @@ class ChatAdapter(private val chatList: MutableList<ChatObject?>?, private val c
                         val second = String.format("%02d", Integer.valueOf(chatList[position]!!.audio_length!!) % 60)
                         holder.mMessage.text = "$minute:$second"
                     }
-                    holder.begin_audio.setTextColor(Color.parseColor("#292929"))
+                    holder.beginAudio.setTextColor(Color.parseColor("#292929"))
                     holder.mMessage.setTextColor(Color.parseColor("#292929"))
                     holder.mchk3.visibility = View.GONE
                     holder.mchk.visibility = View.VISIBLE
@@ -136,14 +137,14 @@ class ChatAdapter(private val chatList: MutableList<ChatObject?>?, private val c
                     params.addRule(RelativeLayout.END_OF, holder.mchk2.id)
                     params.addRule(RelativeLayout.ALIGN_BOTTOM, holder.mchk2.id)
                     holder.mchk2.background = ContextCompat.getDrawable(context!!, R.drawable.chat_2)!!
-                    holder.mImage_opposite.visibility = View.VISIBLE
-                    Glide.with(context).load(chatList[position]!!.profileImageUrl).into(holder.mImage_opposite)
+                    holder.imageOpposite.visibility = View.VISIBLE
+                    Glide.with(context).load(chatList[position]!!.profileImageUrl).into(holder.imageOpposite)
                 }
                 else -> {
                     holder.mMessage.text = chatList[position]!!.message
-                    holder.button_audio.visibility = View.GONE
-                    holder.CD!!.visibility = View.GONE
-                    holder.begin_audio.visibility = View.GONE
+                    holder.buttonAudio.visibility = View.GONE
+                    holder.cd.visibility = View.GONE
+                    holder.beginAudio.visibility = View.GONE
                     holder.mMessage.setTextColor(Color.parseColor("#292929"))
                     holder.mchk3.visibility = View.GONE
                     holder.mchk.visibility = View.VISIBLE
@@ -155,8 +156,8 @@ class ChatAdapter(private val chatList: MutableList<ChatObject?>?, private val c
                     params.addRule(RelativeLayout.END_OF, holder.mchk2.id)
                     params.addRule(RelativeLayout.ALIGN_BOTTOM, holder.mchk2.id)
                     holder.mchk2.background = ContextCompat.getDrawable(context!!, R.drawable.chat_2)
-                    holder.mImage_opposite.visibility = View.VISIBLE
-                    Glide.with(context).load(chatList[position]!!.profileImageUrl).into(holder.mImage_opposite)
+                    holder.imageOpposite.visibility = View.VISIBLE
+                    Glide.with(context).load(chatList[position]!!.profileImageUrl).into(holder.imageOpposite)
                 }
             }
         }
