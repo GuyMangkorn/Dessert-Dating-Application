2e9ab7fbacd0cb2b3030743afe2960b4917f6412
katawong0@gmail.com <sotgame123>
2020-10-17 16:16:03 +0700

    Add activity_question.xml to project

---
+++ app/src/main/java/com/jabirdeveloper/tinderswipe/Register/QuestionActivity.kt
@@ -0,0 +1,12 @@
+package com.jabirdeveloper.tinderswipe.Register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jabirdeveloper.tinderswipe.R
+
+class QuestionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question)
+    }
+}
